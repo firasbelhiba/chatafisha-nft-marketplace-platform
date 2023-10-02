3ed8d100886511df3e971929e36b8087933d7d34
NadaLabidi1 <nada.labidi@esprit.tn>
2023-10-02 11:59:39 +0100

    some display conditioning

--- src/components/Collections/Collections.js
+++ src/components/Collections/Collections.js
@@ -27,48 +27,28 @@ const Collections = () => {
 
   return (
     <section className="popular-collections-area">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            {/* Intro */}
-            <div className="intro d-flex justify-content-between align-items-end m-0">
-              <div className="intro-content">
-                <span>{initData.pre_heading}</span>
-                <h3 className="mt-3 mb-0">{initData.heading}</h3>
+      {collectionData.length > 0 ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* Intro */}
+              <div className="intro d-flex justify-content-between align-items-end m-0">
+                <div className="intro-content">
+                  <span>{initData.pre_heading}</span>
+                  <h3 className="mt-3 mb-0">{initData.heading}</h3>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row items">
-          {collectionData.map((item, idx) => {
-            return (
-              <div key={`cd_${idx}`} className="col-12 col-sm-6 col-lg-3 item">
-                <div className="card no-hover text-center">
-                  <div className="image-over">
-                    <Link
-                      to={`/item-details/${encodeURIComponent(
-                        JSON.stringify(item)
-                      )}`}
-                      onClick={() => {
-                        handleLinkClick(
-                          `/item-details/${encodeURIComponent(
-                            JSON.stringify(item)
-                          )}`
-                        );
-                      }}
-                      state={{ stateParam: item }}
-                    >
-                      <img
-                        className="card-img-top"
-                        src={item.metadata.media}
-                        alt=""
-                      />
-                    </Link>
-                  </div>
-                  {/* Card Caption */}
-                  <div className="card-caption col-12 p-0">
-                    {/* Card Body */}
-                    <div className="card-body mt-4">
+          <div className="row items">
+            {collectionData.map((item, idx) => {
+              return (
+                <div
+                  key={`cd_${idx}`}
+                  className="col-12 col-sm-6 col-lg-3 item"
+                >
+                  <div className="card no-hover text-center h-100">
+                    <div className="image-cover">
                       <Link
                         to={`/item-details/${encodeURIComponent(
                           JSON.stringify(item)
@@ -80,37 +60,63 @@ const Collections = () => {
                             )}`
                           );
                         }}
-                        state={{ stateParam: item }} // Pass the item data as the state
+                        state={{ stateParam: item }}
                       >
-                        <h5 className="mb-2">{item.metadata.title}</h5>
+                        <img
+                          className="card-img-top img-fluid"
+                          src={item.metadata.media}
+                          alt=""
+                          style={{ height: "200px", objectFit: "cover" }}
+                        />
                       </Link>
-                      <span>{item.content}</span>
                     </div>
-
-                    <Link
-                      to={`/item-details/${encodeURIComponent(
-                        JSON.stringify(item)
-                      )}`}
-                      onClick={() => {
-                        handleLinkClick(
-                          `/item-details/${encodeURIComponent(
+                    {/* Card Caption */}
+                    <div className="card-caption col-12 p-0">
+                      {/* Card Body */}
+                      <div className="card-body mt-4">
+                        <Link
+                          to={`/item-details/${encodeURIComponent(
                             JSON.stringify(item)
-                          )}`
-                        );
-                      }}
-                      state={{ stateParam: item }}
-                      className="btn btn-bordered-white btn-smaller mt-3"
-                    >
-                      <i className="icon-handbag mr-2" />
-                      Transfer
-                    </Link>
+                          )}`}
+                          onClick={() => {
+                            handleLinkClick(
+                              `/item-details/${encodeURIComponent(
+                                JSON.stringify(item)
+                              )}`
+                            );
+                          }}
+                          state={{ stateParam: item }} // Pass the item data as the state
+                        >
+                          <h5 className="mb-2">{item.metadata.title}</h5>
+                        </Link>
+                        <span>{item.content}</span>
+                      </div>
+
+                      <Link
+                        to={`/item-details/${encodeURIComponent(
+                          JSON.stringify(item)
+                        )}`}
+                        onClick={() => {
+                          handleLinkClick(
+                            `/item-details/${encodeURIComponent(
+                              JSON.stringify(item)
+                            )}`
+                          );
+                        }}
+                        state={{ stateParam: item }}
+                        className="btn btn-bordered-white btn-smaller mt-3"
+                      >
+                        <i className="icon-handbag mr-2" />
+                        Transfer
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : null}
     </section>
   );
 };
--- src/components/Explore/ExploreThree.js
+++ src/components/Explore/ExploreThree.js
@@ -14,52 +14,28 @@ const ExploreThree = ({ tokens }) => {
   };
   return (
     <section className="explore-area">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            {/* Intro */}
-            <div className="intro d-flex justify-content-between align-items-end m-0">
-              <div className="intro-content">
-                <span>{initData.pre_heading}</span>
-                <h3 className="mt-3 mb-0">{initData.heading}</h3>
+      {tokens ? (
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              {/* Intro */}
+              <div className="intro d-flex justify-content-between align-items-end m-0">
+                <div className="intro-content">
+                  <span>{initData.pre_heading}</span>
+                  <h3 className="mt-3 mb-0">{initData.heading}</h3>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="row items explore-items" key={tokens}>
-          {tokens.map((item, idx) => {
-            return (
-              <div
-                key={`edth_${idx}`}
-                className="col-12 col-sm-6 col-lg-3 item explore-item"
-              >
-                <div className="card h-100">
-                  <div className="image-cover">
-                    <Link
-                      to={`/item-details/${encodeURIComponent(
-                        JSON.stringify(item)
-                      )}`}
-                      onClick={() => {
-                        handleLinkClick(
-                          `/item-details/${encodeURIComponent(
-                            JSON.stringify(item)
-                          )}`
-                        );
-                      }}
-                      state={{ stateParam: item }}
-                    >
-                      <img
-                        className="card-img-top img-fluid"
-                        src={item.metadata.media}
-                        alt=""
-                        style={{ height: "200px", objectFit: "cover" }}
-                      />
-                    </Link>
-                  </div>
-                  {/* Card Caption */}
-                  <div className="card-caption col-12 p-0">
-                    {/* Card Body */}
-                    <div className="card-body">
+          <div className="row items explore-items" key={tokens}>
+            {tokens.map((item, idx) => {
+              return (
+                <div
+                  key={`edth_${idx}`}
+                  className="col-12 col-sm-6 col-lg-3 item explore-item"
+                >
+                  <div className="card h-100">
+                    <div className="image-cover">
                       <Link
                         to={`/item-details/${encodeURIComponent(
                           JSON.stringify(item)
@@ -73,10 +49,18 @@ const ExploreThree = ({ tokens }) => {
                         }}
                         state={{ stateParam: item }}
                       >
-                        <h5 className="mb-0">{item.metadata.title}</h5>
+                        <img
+                          className="card-img-top img-fluid"
+                          src={item.metadata.media}
+                          alt=""
+                          style={{ height: "200px", objectFit: "cover" }}
+                        />
                       </Link>
-                      <div className="seller d-flex align-items-center my-3">
-                        <span>Owned By</span>
+                    </div>
+                    {/* Card Caption */}
+                    <div className="card-caption col-12 p-0">
+                      {/* Card Body */}
+                      <div className="card-body">
                         <Link
                           to={`/item-details/${encodeURIComponent(
                             JSON.stringify(item)
@@ -90,35 +74,53 @@ const ExploreThree = ({ tokens }) => {
                           }}
                           state={{ stateParam: item }}
                         >
-                          <h6 className="ml-2 mb-0">{item.owner_id}</h6>
+                          <h5 className="mb-0">{item.metadata.title}</h5>
+                        </Link>
+                        <div className="seller d-flex align-items-center my-3">
+                          <span>Owned By</span>
+                          <Link
+                            to={`/item-details/${encodeURIComponent(
+                              JSON.stringify(item)
+                            )}`}
+                            onClick={() => {
+                              handleLinkClick(
+                                `/item-details/${encodeURIComponent(
+                                  JSON.stringify(item)
+                                )}`
+                              );
+                            }}
+                            state={{ stateParam: item }}
+                          >
+                            <h6 className="ml-2 mb-0">{item.owner_id}</h6>
+                          </Link>
+                        </div>
+
+                        <Link
+                          to={`/item-details/${encodeURIComponent(
+                            JSON.stringify(item)
+                          )}`}
+                          onClick={() => {
+                            handleLinkClick(
+                              `/item-details/${encodeURIComponent(
+                                JSON.stringify(item)
+                              )}`
+                            );
+                          }}
+                          state={{ stateParam: item }}
+                          className="btn btn-bordered-white btn-smaller mt-3"
+                        >
+                          <i className="icon-rocket mr-2" />
+                          Details
                         </Link>
                       </div>
-
-                      <Link
-                        to={`/item-details/${encodeURIComponent(
-                          JSON.stringify(item)
-                        )}`}
-                        onClick={() => {
-                          handleLinkClick(
-                            `/item-details/${encodeURIComponent(
-                              JSON.stringify(item)
-                            )}`
-                          );
-                        }}
-                        state={{ stateParam: item }}
-                        className="btn btn-bordered-white btn-smaller mt-3"
-                      >
-                        <i className="icon-rocket mr-2" />
-                        Details
-                      </Link>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
+      ) : null}
     </section>
   );
 };
